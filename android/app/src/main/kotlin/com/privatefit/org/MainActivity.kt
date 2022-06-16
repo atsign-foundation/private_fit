2f6d72b648eace703eb1a88d234db563d3e3b953
Kelvin Zawadi <kelvin.vitone@gmail.com>
2022-06-16 15:45:37 +0300

    fresh app from at_app cli and vgv cli

---
+++ android/app/src/main/kotlin/com/privatefit/org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.privatefit.org.private_fit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:private_fit/app/app.dart';
+import 'package:private_fit/bootstrap.dart';
+import 'package:pub_api_client/pub_api_client.dart';
+
+void main()  {
+  bootstrap(() => const App());
+}
